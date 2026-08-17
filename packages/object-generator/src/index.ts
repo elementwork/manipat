52b26177074fb3e34528d4c1c8442e402a9f2d5c
elementwork <elementworkinc@gmail.com>
2026-08-17 17:21:25 -0400

    export complex aperture templates

--- packages/object-generator/src/index.ts
+++ packages/object-generator/src/index.ts
@@ -1,3 +1,4 @@
 export * from "./templates/aperture-templates.js";
+export * from "./templates/aperture-complex-templates.js";
 export * from "./templates/tfe-templates.js";
 export * from "./types.js";
