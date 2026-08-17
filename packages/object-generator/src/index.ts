6a281bfa165708cb9d2b2994bb09cc0dadee7013
elementwork <elementworkinc@gmail.com>
2026-08-17 19:27:09 -0400

    export advanced PAT model banks

--- packages/object-generator/src/index.ts
+++ packages/object-generator/src/index.ts
@@ -1,5 +1,7 @@
 export * from "./templates/aperture-templates.js";
 export * from "./templates/aperture-complex-templates.js";
 export * from "./templates/aperture-faceted-templates.js";
+export * from "./templates/aperture-advanced-templates.js";
 export * from "./templates/tfe-templates.js";
+export * from "./templates/tfe-advanced-templates.js";
 export * from "./types.js";
